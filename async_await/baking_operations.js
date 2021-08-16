fe6fcb58df0a8fbfbc71fb0701721d31fadb4bca
Richard L. Jackson <denezt@users.noreply.github.com>
2021-08-16 20:13:16 +0200

    Update baking_operations.js

--- async_await/baking_operations.js
+++ async_await/baking_operations.js
@@ -22,7 +22,32 @@ function time(ms){
 
 async function cafe(){
   try {
+    await time(2000);
+    console.log(`${supplies.filling[0]} was chosen.`);
 
+    await time(0000);
+    console.log("starting to prepare pie");
+
+    await time(2000);
+    console.log("The filling has been poured.");
+
+    await time(2000);
+    console.log(`Placing ${supplies.texture[1]} ingredients in a ${supplies.form[1]} form.`);
+
+    await time(2000);
+    console.log(`Oven was preheated.`);
+
+    await time(2000);
+    console.log(`Placed ${supplies.form[1]} form in oven.`);
+
+    await time(1500);
+    console.log(`Pie was baked for 10 mins, taking out of oven.`);
+
+    await time(1500);
+    console.log(`Placing ${supplies.toppings[2]} on pie.`);
+
+    await time(1500);
+    console.log(`Serve pie to customer.`);
   } catch (error) {
     console.log("Unable to serve customers",error);
   } finally {
